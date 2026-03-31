e48fa1fbf43d585df8b920352c43d9af0312963d
Saamia Aziz <saamiaaziz6@gmail.com>
2026-03-31 14:26:11 +0500

    updated

--- script.js
+++ script.js
@@ -23,17 +23,10 @@ navItems.forEach(item => {
     });
 });
 
-if (menuBtn) {
-    menuBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
-}
-
-
 // Mobile menu toggle
-// menuBtn.addEventListener('click', () => {
-//    navLinks.classList.toggle('show');
-//});
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
 
 // --- Typing Effect ---
 const typeText = ["Data Scientist", "Power Bi Developer", "Python Developer", "Graphic Designer"];
